69caf6571edbe4135abab07edd51f394f8c8aaad
Sandeepkumar <sandeepkumarrk1112@gmail.com>
2025-09-17 02:34:25 +0530

    232. Added Resilence4j Dependency in order pom.xml and in order service - addToCart method - implementing Circuit Breaker - It require when  service-A is dependent on a service-B and if service-B is down then , instead for throwing fail, we can implement circuit breaker
    We wrote some Config properties in yml and Based on name we mention while writing config, we need to use that name while adding @CircuitBreaker(name="productservice")

---
+++ 8.FaultToleranceAndCircuitBreaker/order/src/main/java/com/ecommerce/order/dtos/UserResponse.java
@@ -0,0 +1,20 @@
+package com.ecommerce.order.dtos;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    private String  id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private UserRole role;
+
+    private AddressDTO address;
+}
